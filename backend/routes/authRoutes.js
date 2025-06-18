ce2290c115283f34a60f5c9e78311eb149290a63
VITOR MARINHO DA SILVA DOS SANTOS <vitor_ms_santos@estudante.sesisenai.org.br>
2025-06-18 16:27:37 -0300

    Sexto commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,11 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { registrar, login } = require('../controllers/authController');
 
-<<<<<<< HEAD
 router.post('/registrar', registrar);
-=======
 router.post("/registrar", registrar); 
->>>>>>> 309f41a (Quarto commit)
 router.post('/login', login);
 
 module.exports = router;
